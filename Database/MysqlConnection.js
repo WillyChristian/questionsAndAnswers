8c55d9574acb0aa7dc2a33ed8cdf8ddf1291e0cc
Willy Christian <crisaort@gmail.com>
2020-04-14 02:34:35 -0300

    .
    .

--- Database/MysqlConnection.js
+++
@@ -1,8 +0,0 @@
-const sequelize = require('sequelize')
-
-const connection = new sequelize("Questions", "root", "1321", {
-  host: "localhost",
-  dialect: 'mysql'
-});
-
-module.exports = connection
